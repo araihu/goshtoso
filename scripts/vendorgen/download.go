858d0d31179e6493eab68a6ca40680147cec6a9f
Guilherme de Castro <guilherme.castro@totvs.com.br>
2026-06-02 04:31:26 -0300

    ci: fail on stale vendor_gen.go (vendorgen -check)

--- scripts/vendorgen/download.go
+++ scripts/vendorgen/download.go
@@ -41,7 +41,7 @@ func fetch(url string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
+	defer func() { _ = resp.Body.Close() }()
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("GET %s: status %d", url, resp.StatusCode)
 	}
